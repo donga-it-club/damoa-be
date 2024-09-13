8c9194dcb766d4a7c7b3fbe23270492f9ceb4b14
w1shope <wishhopelee@gmail.com>
2024-09-13 21:49:49 +0900

    chore: MemberController API 문서화

--- src/main/java/com/donga/damoa/domain/member/dto/SignUpRequest.java
+++ src/main/java/com/donga/damoa/domain/member/dto/SignUpRequest.java
@@ -1,5 +1,6 @@
 package com.donga.damoa.domain.member.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AccessLevel;
@@ -14,22 +15,29 @@ public class SignUpRequest {
 
     @Email(message = "이메일 형식에 맞게 입력해주세요.")
     @NotBlank(message = "이메일은 필수 입력값입니다.")
+    @Schema(description = "이메일", example = "test@test.com")
     private String email; // 이메일
 
     @NotBlank(message = "비밀번호는 필수 입력값입니다.")
+    @Schema(description = "비밀번호", example = "test")
     private String password; // 비밀번호
 
     @NotBlank(message = "이름은 필수 입력값입니다.")
+    @Schema(description = "이름", example = "홍길동")
     private String memberName; // 이름
 
     @NotBlank(message = "주전공은 필수 입력값입니다.")
+    @Schema(description = "주전공", example = "컴퓨터공학과")
     private String majorName; // 주전공
 
+    @Schema(description = "부전공", example = "경영정보학과")
     private String minorName; // 부전공
 
     @NotBlank(message = "재학 여부는 필수 입력값입니다.")
+    @Schema(description = "재학 여부", example = "ENROLLED")
     private String enrollmentStatus; // 재학 여부
 
+    @Schema(description = "PR 링크", example = "https://server-technology.tistory.com")
     private String prLink;
 
 }
